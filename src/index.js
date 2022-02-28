06ef2692c18e2492d2c9a3a5b3d5b853aa67094b
Alla <allakakhanovich@gmail.com>
2022-02-28 16:49:19 +0300

    fix: add getBrackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+// module.exports = function check(str, bracketsConfig) {
+//   // your solution
+// }
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = getBrackets(bracketsConfig);
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '|' && str[i] === str[str.length-1])        {
+      stack.push(str[i]);
+    }
+    if (isClose(str[i])) {
+      if (brackets[str[i]] !== stack.pop()){
+        return false;
+      } 
+    } else {
+      stack.push(str[i]);
+    }
+  }
+  return stack.length === 0;
 }
+
+function isClose(cur) {
+  return [')', ']', '}', '|'].indexOf(cur) > -1;
+}
+
+function getBrackets(bracketsConfig) {
+  let objBr = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    objBr[bracketsConfig[i][1]] = bracketsConfig[i][1];
+  }
+
+  return objBr;
+}
+
+
+
